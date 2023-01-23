406ddb95895bf16b326f060294e89040f82a3309
KDOXG <kevin.20pereira@gmail.com>
2023-01-22 22:05:29 -0300

    Jogo do tutorial, exemplo de projeto estruturado

---
+++ Exemplo/Main/Main.gd
@@ -0,0 +1,59 @@
+extends Node
+
+export(PackedScene) var mob_scene
+var score
+
+func _ready():
+	randomize()
+
+#func _process(delta):
+#	pass
+
+func game_over():
+	$HUD.show_game_over()
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$Music.stop()
+	$DeathSound.play()
+
+func new_game():
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$Music.play()
+	$DeathSound.stop()
+	get_tree().call_group("mobs", "queue_free")
+
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
+
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Set the mob's position to a random location.
+	mob.position = mob_spawn_location.position
+
+	# Add some randomness to the direction.
+	# Affects graphics
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	# Choose the velocity for the mob.
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
