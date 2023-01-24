44c21f10f41e0d2b0f67dae6ca02c0459cd4ea7b
mikurindotc <pedrobene13@gmail.com>
2023-01-23 21:01:44 -0300

    First commit of Pong

---
+++ Pong/Scenes/GameObjects/Player1.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+signal hit
+
+export var speed = 500
+var player_height = 144
+var screen_size
+var velocity
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	velocity = Vector2.ZERO
+	move_player()
+	clamp_move(delta)
+
+func move_player():
+	if Input.is_action_pressed("move_up_p1"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down_p1"):
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+func clamp_move(delta):
+	position += velocity * delta
+	position.y = clamp(position.y, 0, screen_size.y - player_height)
+
+func _on_Area2D_body_entered(body):
+	emit_signal("hit")
